ef6985ca9b7f4bacd3134773360d461e870b5add
tholmes741 <holmesiii.thomas@gmail.com>
2015-12-15 11:39:17 -0800

    react rendering properly

--- frontend/audio_water_vapor.jsx
+++ frontend/audio_water_vapor.jsx
@@ -1,2 +1,15 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+
+// var test = React.creatClass({
+//   render: function() {
+//     return (
+//       <div>react stuff</div>
+//     );
+//   }
+// });
+
+document.addEventListener('DOMContentLoaded', function(){
+  var content = document.getElementById('content');
+  ReactDOM.render(<div>hi</div>, content);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,10 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel'
+        loader: 'babel',
+        query: {
+          presets: ['react']
+        }
       }
     ]
   },
